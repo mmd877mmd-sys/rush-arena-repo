d45aec6ed73ca693241270075b517ece2d5cc548
redoy hossen <redoy.hossen.emon@mail.com>
2025-11-08 11:31:09 +0600

    add config.json

--- hooks/useRemoteConfig.js
+++ hooks/useRemoteConfig.js
@@ -15,7 +15,9 @@ export default function useRemoteConfig() {
   useEffect(() => {
     async function fetchConfig() {
       try {
-        const res = await axios.get("https://myserver.com/config.json");
+        const res = await axios.get(
+          `${process.env.NEXT_PUBLIC_WEB_URL}/config.json`
+        );
         setConfig(res.data);
       } catch (err) {
         console.error("Failed to fetch config:", err);
