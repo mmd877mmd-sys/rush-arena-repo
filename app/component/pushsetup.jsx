3cf7232a0f82a71a5748bb5b778b0264f9d40f67
redoy hossen <redoy.hossen.emon@mail.com>
2025-11-27 11:21:20 +0600

    added push configure

--- app/component/pushsetup.jsx
+++ app/component/pushsetup.jsx
@@ -30,10 +30,10 @@ export default function AppInit() {
 
   const saveToken = async (t) => {
     try {
-      const res = await axios.post("/api/save-token", { token: t });
+      const res = await axios.post("/api/saveToken", { token: t });
 
-      if (!res.data?.success) {
-        showToast("error", "Failed to save token to server!");
+      if (res.data?.success) {
+        showToast("success", "Notification accessed ");
       }
     } catch (err) {
       console.error("Error saving token:", err);
